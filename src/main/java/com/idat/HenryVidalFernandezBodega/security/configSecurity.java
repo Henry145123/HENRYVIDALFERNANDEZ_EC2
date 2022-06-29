be09200b2983500751c2d0145eb9db49e9174ca4
Henry Vidal Fernandez <henry145123@gmail.com>
2022-06-28 23:34:38 -0500

    EC2 HENRY VIDAL FERNANDEZ

--- src/main/java/com/idat/HenryVidalFernandezBodega/security/configSecurity.java
+++ src/main/java/com/idat/HenryVidalFernandezBodega/security/configSecurity.java
@@ -1,46 +1,52 @@
 package com.idat.HenryVidalFernandezBodega.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
+
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 @Configuration
 @EnableWebSecurity
 public class configSecurity extends WebSecurityConfigurerAdapter {
 
 	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("profesor").password(encriptado().encode("123") ).roles("ADMIN");
-		auth.inMemoryAuthentication().withUser("estudiante").password(encriptado().encode("123")).roles("USER");
-
-
+		
+		auth.inMemoryAuthentication().withUser("henry").password(passwordEncoder().encode("he123456")).roles("ADMIN");
+		
+		auth.inMemoryAuthentication().withUser("Juan").password(passwordEncoder().encode("ju123456")).roles("USER");
+	
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
 		http.authorizeRequests()
-			.antMatchers("/**").access("hasRole('ADMIN')")
+			//.antMatchers("/producto/v1/listar").permitAll()
 			.antMatchers("/producto/v1/listar").access("hasRole('USER')")
-			.and()
-			.httpBasic()
-			.and()
-			.csrf().disable();
-	}
-	
-	@Bean
-	public PasswordEncoder encriptado() {
-		return new BCryptPasswordEncoder();
+			.antMatchers("/producto/v1/actualizar").access("hasRole('ADMIN')")
+			.antMatchers("/producto/v1/guardar").access("hasRole('ADMIN')")
+			.antMatchers("/producto/v1/eliminar").access("hasRole('ADMIN')")
+			.antMatchers("/producto/v1/listar/**").access("hasRole('ADMIN')")
+			.antMatchers("/bodega/v1/**").access("hasRole('ADMIN')")
+			.antMatchers("/cliente/v1/**").access("hasRole('ADMIN')")
+			.antMatchers("/usuario/v1/**").access("hasRole('ADMIN')");
+			
+		
+		http.authorizeRequests().and().httpBasic();
+		http.authorizeRequests().and().csrf().disable();
 	}
 
 }
